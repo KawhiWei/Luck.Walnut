0e54ecf809f4f7ecc76b2c5ee989ee3db977f410
王泽威 <1585955375@qq.com>
2022-07-14 21:56:39 +0800

    feat : 添加grpc处理长链接代码

--- src/Luck.Walnut.Api/GrpcServices/LuCatGrpcService.cs
+++ src/Luck.Walnut.Api/GrpcServices/LuCatGrpcService.cs
@@ -10,17 +10,18 @@ using MediatR;
 namespace Luck.Walnut.Api.GrpcServices;
 
 [AutoMapGrpcService]
-public class LuCatGrpcService : LuCat.LuCatBase
+public class LuCatGrpcService : LuCat.LuCatBase, INotificationHandler<AppConfigurationEvent>
 {
     private readonly ILogger<LuCatGrpcService> _logger;
-    private readonly IClientMananger _clientManager;
     private readonly IApplactionClientConcurrentQueue _applactionClientConcurrentQueue;
+    private readonly ITest _test;
 
-    public LuCatGrpcService(IClientMananger clientManager,IApplactionClientConcurrentQueue applactionClientConcurrentQueue, ILogger<LuCatGrpcService> logger)
+    public LuCatGrpcService(IApplactionClientConcurrentQueue applactionClientConcurrentQueue,
+        ILogger<LuCatGrpcService> logger, ITest test)
     {
-        _clientManager = clientManager;
         _applactionClientConcurrentQueue = applactionClientConcurrentQueue;
         _logger = logger;
+        _test = test;
     }
 
     /// <summary>
@@ -37,21 +38,47 @@ public class LuCatGrpcService : LuCat.LuCatBase
             //将要洗澡的猫加入队列
             Console.WriteLine($"Cat {requestStream.Current.Id} Enqueue.");
         }
+
+        var clientId = Guid.NewGuid().ToString();
+        _test.AddResponseStream("test", clientId, responseStream, context);
+        context.CancellationToken.Register(() => { _test.RemoveResponseStream("test", clientId); });
         while (true)
         {
-            if (_applactionClientConcurrentQueue.ConcurrentQueue.TryDequeue(out var str))
-            {
-                await responseStream.WriteAsync(new BathTheCatResp() { Message = $"铲屎的成功给一只{str}洗了澡！" });
-            }
             await Task.Delay(1000);
         }
+        // ReSharper disable once FunctionNeverReturns
     }
-    
+
     public override Task<CountCatResult> Count(Empty request, ServerCallContext context)
     {
         return Task.FromResult(new CountCatResult()
         {
             Count = 80
         });
+    }
+
+    public async Task Handle(AppConfigurationEvent notification, CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask;
+        // var conn = _test.GetResponseStream("test");
+        // if (conn is not null)
+        // {
+        //     try
+        //     {
+        //         if (conn.Value.responseStream is not null)
+        //         {
+        //             if (conn.Value.context.CancellationToken.IsCancellationRequested)
+        //             {
+        //                 await conn.Value.responseStream.WriteAsync(new BathTheCatResp()
+        //                     { Message = $"铲屎的成功给一只[{notification.AppId}+{Guid.NewGuid()}]洗了澡！" });
+        //             }
+        //         }
+        //     }
+        //     catch (Exception e)
+        //     {
+        //         Console.WriteLine(e);
+        //         throw;
+        //     }
+        // }
     }
 }
